ad85500e07a18a1596947aa67b90b48a71e02314
ajeseung <145750431+ajeseung@users.noreply.github.com>
2025-10-10 16:58:04 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,11 +1,34 @@
+# def solution(array, commands):
+#     result = []
+    
+#     # commands의 각 명령을 처리
+#     for command in commands:
+#         i, j, k = command
+#         # i번째부터 j번째까지 자르고 정렬한 후 k번째 숫자 추출
+#         sliced_array = sorted(array[i-1:j])
+#         result.append(sliced_array[k-1])
+    
+#     return result
+
+
+
+
 def solution(array, commands):
     result = []
     
-    # commands의 각 명령을 처리
     for command in commands:
-        i, j, k = command
-        # i번째부터 j번째까지 자르고 정렬한 후 k번째 숫자 추출
+        i,j,k = command
         sliced_array = sorted(array[i-1:j])
         result.append(sliced_array[k-1])
-    
+
     return result
+
+
+
+
+
+
+
+
+
+
