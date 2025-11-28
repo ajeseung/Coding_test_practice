750ee18054ae7abc9e3185f02091fd3cb1e51087
ajeseung <145750431+ajeseung@users.noreply.github.com>
2025-11-28 11:01:12 +0900

    [level 0] Title: 배열 만들기 2, Time: 638.82 ms, Memory: 9.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,12 @@
+def solution(l, r):
+    answer = []
+    for i in range(l, r+1):
+        s = str(i)
+        if all(ch in ('0', '5') for ch in s):
+            answer.append(i)
+            
+    if not answer:
+        return [-1]
+    
+    return answer
+    
