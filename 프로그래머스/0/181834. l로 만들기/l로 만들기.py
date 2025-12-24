ac0f104615b810e4fea9bbc81c42708a4234408c
ajeseung <145750431+ajeseung@users.noreply.github.com>
2025-12-24 11:42:00 +0900

    [level 0] Title: l로 만들기, Time: 8.42 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+    
+    for c in myString:
+        if c < 'l':
+            answer += 'l'
+        else:
+            answer += c
+            
+    return answer
