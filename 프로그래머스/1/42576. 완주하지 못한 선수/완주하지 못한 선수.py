e4f9f0e5e6c6f5d454c4fb05202bd77842c5dd9d
ajeseung <145750431+ajeseung@users.noreply.github.com>
2026-01-05 13:48:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.11 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,25 @@
+
+
 def solution(participant, completion):
     participant.sort()
     completion.sort()
     
-    for p,c in zip(participant, completion):
-        if p != c:
-            return p
+
+
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
         
     return participant[-1]
+
+
+
+
+
+
+
+
+
+
+
+
