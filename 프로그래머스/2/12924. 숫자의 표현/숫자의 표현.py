88807fa2824d4fec6c189c6e9a67753a996f8912
ajeseung <145750431+ajeseung@users.noreply.github.com>
2026-01-05 10:07:41 +0900

    [level 2] Title: 숫자의 표현, Time: 0.50 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,8 +1,16 @@
+# def solution(n):
+#     count = 0
+#     k = 1
+#     while k * (k - 1) // 2 < n:
+#         if (n - (k * (k - 1) // 2)) % k == 0:
+#             count += 1
+#         k += 1
+#     return count
+
 def solution(n):
     count = 0
-    k = 1
-    while k * (k - 1) // 2 < n:
-        if (n - (k * (k - 1) // 2)) % k == 0:
+    for i in range(1, n + 1, 2):  # 홀수만 확인
+        if n % i == 0:
             count += 1
-        k += 1
     return count
+
