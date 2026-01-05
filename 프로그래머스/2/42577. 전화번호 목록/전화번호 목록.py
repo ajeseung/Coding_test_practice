0c0329648d0e26b4b0e02274649e2874b81aa548
ajeseung <145750431+ajeseung@users.noreply.github.com>
2026-01-05 13:54:04 +0900

    [level 2] Title: 전화번호 목록, Time: 97.79 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,33 @@
+# def solution(phone_book):
+#     phone_set = set(phone_book)  # 1. 모든 번호를 set에 저장
+
+#     for number in phone_book:
+#         # 2. 접두어를 1자리부터 끝-1자리까지 잘라가며 확인
+#         for i in range(1, len(number)):
+#             prefix = number[:i]
+#             if prefix in phone_set:
+#                 return False  # 접두어가 실제 다른 번호로 존재하면 False
+
+#     return True  # 접두어가 없으면 True
+
+
 def solution(phone_book):
-    phone_set = set(phone_book)  # 1. 모든 번호를 set에 저장
+    phone_book.sort()
 
-    for number in phone_book:
-        # 2. 접두어를 1자리부터 끝-1자리까지 잘라가며 확인
-        for i in range(1, len(number)):
-            prefix = number[:i]
-            if prefix in phone_set:
-                return False  # 접두어가 실제 다른 번호로 존재하면 False
+    for i in range(len(phone_book) - 1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
 
-    return True  # 접두어가 없으면 True
+    return True
+
+
+
+
+
+
+
+
+
+
+
+
